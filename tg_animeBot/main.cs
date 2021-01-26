74c09de5b6d07c7badd1a134f8e92fa5785c51ac
Shoukox <58671746+Shoukox@users.noreply.github.com>
2021-01-27 02:09:18 +0500

    Update main.cs

--- tg_animeBot/main.cs
+++ tg_animeBot/main.cs
@@ -7,7 +7,7 @@ namespace tg_animeBot
     {
         static void Main(string[] args)
         {
-            string token = "1328219094:AAFjFsz80--0N_hT1K1-FRXVpkQvyQdGAWA";
+            string token = "YOUR_TOKEN";
             while (true)
             {
                 BotInstance bot = new BotInstance(token);
